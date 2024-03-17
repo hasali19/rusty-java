5d4cd21ab8d2cbf7d886e10a180f5f6cdf2b66a9
Hasan Mohsin <hasan@hasali.dev>
2024-03-17 12:04:29 +0000

    Cache loaded classes

--- src/vm.rs
+++ src/vm.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufReader};
 use std::iter;
@@ -13,6 +14,7 @@ use crate::reader::ClassReader;
 
 pub struct Vm<'a> {
     arena: &'a Bump,
+    classes: HashMap<&'a str, &'a Class<'a>>,
     pub(crate) stdout: &'a mut dyn io::Write,
     pub(crate) heap: Bump,
 }
@@ -21,12 +23,19 @@ impl<'a> Vm<'a> {
     pub fn new(arena: &'a Bump, stdout: &'a mut dyn io::Write) -> Vm<'a> {
         Vm {
             arena,
+            classes: HashMap::new(),
             stdout,
             heap: Bump::new(),
         }
     }
 
     pub fn load_class_file(&mut self, name: &str) -> eyre::Result<&'a Class<'a>> {
+        let class_name = name.strip_suffix(".class").unwrap_or(name);
+
+        if let Some(class) = self.classes.get(class_name) {
+            return Ok(class);
+        }
+
         let path = Path::new(name).with_extension("class");
         let class_file = self.arena.alloc(
             ClassReader::new(self.arena, BufReader::new(File::open(path)?))
@@ -41,6 +50,8 @@ impl<'a> Vm<'a> {
         {
             self.call_method(class, clinit)?;
         }
+
+        self.classes.insert(class.name(), class);
 
         Ok(class)
     }
